aea06a2907a2f0853227ccef869b3f7edb2c782f
youssef <44912677hd-youssef@users.noreply.github.com>
2018-11-29 12:24:24 -0500

    added method 2 & some tests
    there are a few tests

--- Day2Name.java
+++ Day2Name.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 public class Day2Name{
+
+
+
 	public static void main(String[]args){
 
 
@@ -10,5 +14,35 @@ public class Day2Name{
 		System.out.println("tanav");
 		System.out.println("merge conflict incoming");
 
+		ArrayList<Integer> test = new ArrayList<>();
+
+		test.add(8);
+		test.add(9);
+		test.add(5);
+		test.add(0);
+
+		System.out.println(test);
+		System.out.println(reconstruct(test));
+
+	}
+
+	//method 2 youssef
+	public static int reconstruct (ArrayList<Integer> a){
+
+		int combinary = 0;
+
+		for(int i=0; i<a.size(); i++){
+			int poppy = (a.size()-1) - i;
+			int mult = 1;
+			int value = a.get(i);
+			while(poppy>0){
+				mult*=10;
+				poppy--;
+			}
+			value*=mult;
+			combinary+=value;
+		}
+
+		return combinary;
 	}
 }
