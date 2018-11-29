cabe3810e52f21132648cb011eb0db566b805fe8
Tanavv <42652464+Tanavv@users.noreply.github.com>
2018-11-29 12:37:39 -0500

    Day2 Update
    Method 1 updated to fix bugs involving zeros being a part of the int input.

--- Day2Name.java
+++ Day2Name.java
@@ -22,6 +22,7 @@ public class Day2Name{
 
 	}
 
+	//method 1 tanav
 	public static ArrayList<Integer> covertToArrayList(int x){
 		ArrayList<Integer> list = new ArrayList<Integer>();
 
@@ -29,21 +30,29 @@ public class Day2Name{
 				list.add(x/1000);
 				x = x%1000;
 			}
+			else if(x/1000 == 0)
+				list.add(0);
+
 			if(x >= 100){
 				list.add(x/100);
 				x = x%100;
 			}
+			else if(x/100 == 0)
+				list.add(0);
+
 			if(x >= 10){
 				list.add(x/10);
 				x = x%10;
 			}
+			else if(x/10 == 0)
+				list.add(0);
+
 			if(x >= 0)
 				list.add(x);
+			else if(x/1 == 0)
+				list.add(0);
 
 				return list;
-
-
-
 	}
 
 	//method 2 youssef
