dfa762cedde704eebcca23152d56d15763e73455
Tanavv <42652464+Tanavv@users.noreply.github.com>
2018-11-29 12:16:52 -0500

    Day 2 Practice
    Finialized "convertToArrayList" method and added function to generate number from 0-9999 and prints.

--- Day2Name.java
+++ Day2Name.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
+
 public class Day2Name{
 	public static void main(String[]args){
 
+		int num = (int)(Math.random()*10000);
 
-		System.out.println("youssef");
-		System.out.println("tanav");
-		System.out.println("merge conflict incoming");
+		System.out.println(num);
+		System.out.println(covertToArrayList(num));
+
 	}
+
+	public static ArrayList<Integer> covertToArrayList(int x){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+			if(x >= 1000){
+				list.add(x/1000);
+				x = x%1000;
+			}
+			if(x >= 100){
+				list.add(x/100);
+				x = x%100;
+			}
+			if(x >= 10){
+				list.add(x/10);
+				x = x%10;
+			}
+			if(x >= 0)
+				list.add(x);
+
+				return list;
+
+	}//covertToArrayList
 }
